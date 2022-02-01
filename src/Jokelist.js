8f7f68e0dd942b4b2479bf77bfbe1c106c0a9e13
ZacharyMenefee <zcmenefee02@gmail.com>
2022-02-01 13:06:02 -0500

    Added styling to the joke compone TODO: local storage

--- src/Jokelist.js
+++ src/Jokelist.js
@@ -49,7 +49,7 @@ class JokeList extends Component{
             <div className="JokeList">
             <div className="JokeList-sidebar">
                 <h1 className="JokeList-title"><span>Dad</span> Jokes</h1>
-                <img src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg"></img>
+                <img src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg" alt="Emoji Laughing"></img>
                 <button className="JokeList-getmore">New Jokes</button>
             </div> 
                   <div className="JokeList-jokes">
